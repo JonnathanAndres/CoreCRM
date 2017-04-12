710d857f77d60f79615598c650e098d231e0f47c
HolmesConan <holmesconan@gmail.com>
2017-04-12 15:35:08 +0800

    Add ProfileView frontend.

--- CoreCRM/frontend/webpack.config.js
+++ CoreCRM/frontend/webpack.config.js
@@ -2,38 +2,54 @@
 
 const path = require("path");
 const webpack = require("webpack");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-let outputPath = path.join(__dirname, 'build');
+module.exports = function (env) {
+  env = env || {};
 
-module.exports = {
-  context: path.join(__dirname, "src"),
-  entry: {
-    orgnization: './orgnization.js',
-  },
-  output: {
-    path: outputPath,
-    filename: "[name].bundle.js",
-    publicPath: '/'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: [{
-          loader: "babel-loader",
-          options: { presets: ["es2015"] }
-        }],
-      },
-      {
-        test: /\.vue$/,
-        use: [{
-          loader: "vue-loader"
-        }],
-      },
-      // Loaders for other file types can go here
-    ],
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "build"),
-  },
+  let outputPath = env.production ? path.join(__dirname, '..', 'wwwroot') : path.join(__dirname, 'build');
+
+  let devPlugsins = [
+    new CopyWebpackPlugin([
+      { from: '../node_modules/bootstrap/dist', to: 'lib' },
+      { from: '../node_modules/jquery/dist', to: 'lib/js' },
+      { from: '../node_modules/font-awesome/css', to: 'lib/css' },
+      { from: '../node_modules/font-awesome/fonts', to: 'lib/fonts' },
+      { from: '../node_modules/vue/dist', to: 'lib/js' },
+    ])
+  ];
+
+  return {
+    context: path.join(__dirname, "src"),
+    entry: {
+      orgnization: './orgnization.js',
+    },
+    output: {
+      path: outputPath,
+      filename: "[name].bundle.js",
+      publicPath: '/'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: [{
+            loader: "babel-loader",
+            options: { presets: ["es2015"] }
+          }],
+        },
+        {
+          test: /\.vue$/,
+          use: [{
+            loader: "vue-loader"
+          }],
+        },
+        // Loaders for other file types can go here
+      ],
+    },
+    plugins: env.production ? [] : devPlugsins,
+    devServer: {
+      contentBase: path.join(__dirname, "build"),
+    },
+  };
 };
