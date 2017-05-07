1181a8a82d4229c5d438845f2c6570ddfb553343
Holmes Conan <holmesconan@gmail.com>
2017-05-07 22:10:01 +0800

    Add User, Company, Customer controllers.

---
+++ CoreCRM/Areas/Api/Controllers/CustomerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCRM.Areas.Api.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public CustomerController()
+        {
+
+        }
+    }
+}
