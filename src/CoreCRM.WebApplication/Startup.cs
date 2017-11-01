629be993d6523e024c9e385a688a8ad15d64a0a1
HolmesConan <holmesconan@gmail.com>
2017-11-01 17:29:23 +0800

    WebApplication could serve static files.

--- src/CoreCRM.WebApplication/Startup.cs
+++ src/CoreCRM.WebApplication/Startup.cs
@@ -41,6 +41,8 @@ namespace CoreCRM.WebApplication
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseStaticFiles();
+ 
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
