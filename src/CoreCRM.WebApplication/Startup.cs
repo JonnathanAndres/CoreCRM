c79e85729b46fdada91908c552b21f09e44021cc
HolmesConan <holmesconan@gmail.com>
2017-11-01 17:18:47 +0800

    Remove CoreCRM.Frontend project.

--- src/CoreCRM.WebApplication/Startup.cs
+++ src/CoreCRM.WebApplication/Startup.cs
@@ -41,17 +41,12 @@ namespace CoreCRM.WebApplication
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseExtCore();
- 
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+ 
+            app.UseExtCore();
         }
     }
 }
