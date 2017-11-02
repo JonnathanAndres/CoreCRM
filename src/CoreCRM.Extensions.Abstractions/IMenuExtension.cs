d8e2ef1b7e2f7ea72820a3358eb99e464c8d163f
HolmesConan <holmesconan@gmail.com>
2017-11-02 20:06:26 +0800

    Add CoreCRM.Extensions.Abstractions.

---
+++ src/CoreCRM.Extensions.Abstractions/IMenuExtension.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace CoreCRM.Extensions.Abstractions
+{
+    public interface IMenuExtension
+    {
+        IEnumerator<MenuItem> GetMenu();
+    }
+}
---
+++ src/CoreCRM.Extensions.Abstractions/MenuItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreCRM.Extensions.Abstractions
+{
+    public class MenuItem
+    {
+        public string Name { get; set; }
+        public string Route { get; set; }
+        public string Controller { get; set; }
+        public string Action { get; set; }
+    }
+}
