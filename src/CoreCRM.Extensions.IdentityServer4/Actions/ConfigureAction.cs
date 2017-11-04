40902e0f6f9e809469c153af0895b72c5325b453
HolmesConan <holmesconan@gmail.com>
2017-11-04 16:11:46 +0800

    QuickStart IdentityServer4 is working.

---
+++ src/CoreCRM.Extensions.IdentityServer4/Actions/ConfigureAction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ExtCore.Infrastructure.Actions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace CoreCRM.Extensions.IdentityServer4.Actions
+{
+    public class ConfigureAction : IConfigureAction
+    {
+        public void Execute(IApplicationBuilder applicationBuilder, IServiceProvider serviceProvider)
+        {
+            applicationBuilder.UseIdentityServer();
+        }
+
+        public int Priority => 1000;
+    }
+}
---
+++ src/CoreCRM.Extensions.IdentityServer4/Actions/ConfigureServicesAction.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using ExtCore.Infrastructure.Actions;
+using IdentityServer4.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace CoreCRM.Extensions.IdentityServer4.Actions
+{
+    public class ConfigureServicesAction : IConfigureServicesAction
+    {
+        public void Execute(IServiceCollection serviceCollection, IServiceProvider serviceProvider)
+        {
+            serviceCollection.AddIdentityServer()
+                             .AddDeveloperSigningCredential()
+                             .AddInMemoryApiResources(this.GetApiResources())
+                             .AddInMemoryClients(this.GetClients());
+        }
+        
+        private IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "My API")
+            };
+        }
+        
+        private IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" }
+                }
+            };
+        }
+
+        public int Priority => 1000;
+    }
+}
